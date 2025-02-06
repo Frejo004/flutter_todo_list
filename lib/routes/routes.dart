9e4fa209097a802a9429e68535f17d4dd516292a
Frejus DASSI <frejohope@gmail.com>
2025-02-06 15:34:17 +0100

    Refactor de la classe Routes : correction de la déclaration des routes pour une meilleure lisibilité

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -7,13 +7,12 @@ import 'package:flutter_todo_list/screen/login.dart';
 import 'package:flutter_todo_list/screen/register.dart';
 
 class Routes {
-  final Map<String, WidgetBuilder> _routes = [
+  final Map<String, WidgetBuilder> _routes = 
     {
       '/': (context) => const LoginPage(),
       '/register': (context) => const Register(),
       '/home': (context) => const HomePage(),
-    },
-  ] as Map<String, WidgetBuilder>;
+    };
 
   Map<String, WidgetBuilder> getRoutes() {
     return _routes;
