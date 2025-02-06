46b2a4fd63b5cf2ce69220a965c58a59b520d26c
Frejus DASSI <frejohope@gmail.com>
2025-02-06 10:09:57 +0100

    Remplacement de l'écran d'accueil par l'écran de connexion dans la classe MyApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,8 @@
 import 'package:flutter/material.dart';
-import 'screen/home.dart';
 
-
-
-void main() {
+import 'screen/login.dart';void main() {
   runApp(const MyApp());
 }
-
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -14,19 +10,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        fontFamily: "Roboto",
-      ),
-
-      home:Scaffold(
-        backgroundColor: const Color.fromARGB(255, 255, 254, 255),
-        appBar: AppBar(
-          backgroundColor: const Color.fromARGB(255, 151, 202, 153),
-          title: Center(child: Text("To Do List")),
-        ),
-
-        body: HomePage(),
-      ) ,
+      debugShowCheckedModeBanner: false,
+      home: const LoginPage(),
     );
   }
 }
