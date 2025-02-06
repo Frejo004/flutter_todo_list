958ea7f108b5f5707041c8b282608135ec9d18fc
Frejus DASSI <frejohope@gmail.com>
2025-02-06 11:21:43 +0100

    Ajout de l'écran d'inscription avec gestion des champs utilisateur, email et mot de passe dans l'application

---
+++ lib/screen/register.dart
@@ -0,0 +1,139 @@
+
+
+import 'package:flutter/material.dart';
+import 'package:flutter_todo_list/screen/home.dart';
+import 'package:flutter_todo_list/screen/login.dart';
+
+class Register extends StatefulWidget {
+  const Register({super.key});
+
+  @override
+  State<Register> createState() => _RegisterState();
+}
+
+class _RegisterState extends State<Register> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFF5F5F5),
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 30),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text("Bienvenu à votre ToDo List",
+              style: TextStyle(
+                fontSize: 28,
+                fontWeight: FontWeight.bold,),
+              ),
+              const SizedBox(height: 14),
+
+              const Text(
+                "Inscription",
+                style: TextStyle(fontSize: 16, color: Colors.grey),
+              ),
+
+              const SizedBox(height: 60),
+              
+              // Champ Username
+              TextField(
+                decoration: InputDecoration(
+                  labelText: "Username",
+                  prefixIcon: const Icon(Icons.person, color: Colors.grey),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              
+              
+              // Champ Email
+              TextField(
+                decoration: InputDecoration(
+                  labelText: "Username",
+                  prefixIcon: const Icon(Icons.email_outlined, color: Colors.grey),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              
+
+              // Champ Password
+              TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  labelText: "Password",
+                  prefixIcon: const Icon(Icons.lock, color: Colors.grey),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30),
+                    
+                  ),
+                ),
+              ),
+              const SizedBox(height: 10),
+
+              // Champ PasswordComfirme
+              TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  labelText: "Password",
+                  prefixIcon: const Icon(Icons.lock, color: Colors.grey),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30),
+                    
+                  ),
+                ),
+              ),
+              const SizedBox(height: 10),
+
+              SizedBox(
+                width: double.infinity,
+                height: 50,
+                child: ElevatedButton(
+                  onPressed: () {
+                    // Redirection vers la HomePage
+                     Navigator.pushNamed(
+                      context, '/'
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.purple,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                  ),
+                  child: const Text(
+                    "Inscription",
+                    style: TextStyle(fontSize: 18, color: Colors.white),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              
+                // Bouton "Login"
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text("Aviez-vous déjà un compte?"),
+                    TextButton(
+                      onPressed: () {
+                          Navigator.pushNamed(
+                      context, '/'
+                    );
+                      },
+                      child: const Text("Se connecter 🤩"),
+                    ),
+                  ],
+                ),
+            ],
+          ),),
+      ),
+    );
+  }
+}
+
