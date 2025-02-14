8e07eaa7d6ae9b57c8ba4c5a740a26d431cecb28
Frejus DASSI <frejohope@gmail.com>
2025-02-14 13:59:18 +0100

    Mise à jour du système de routage en remplaçant l'ancien par une nouvelle version et ajustement de la page d'accueil

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,24 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_todo_list/screen/home.dart';
-import 'package:flutter_todo_list/screen/register.dart';
-import 'routes/routes.dart';
+import 'package:flutter_todo_list/routes/routes2.0.dart';
 
+import 'screen/login.dart';
 
-
-import 'screen/login.dart';void main() {
+void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-
   @override
   Widget build(BuildContext context) {
-
-    final Routes routes = Routes() ;
+    // final Routes routes = Routes() ;
 
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      // home: LoginPage(),
       initialRoute: '/',
-      routes: routes.getRoutes(),
+      onGenerateRoute: Routing.generateRoute,
     );
   }
 }
